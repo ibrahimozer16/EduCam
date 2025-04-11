d216a3f835143c31f306fdd330eaf86e5da8dc49
İbrahim Özer <ibrahimozer161616@gmail.com>
2025-04-11 18:38:40 +0300

    setup packages and edited files

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,11 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.assetExts.push('bin');
+defaultConfig.resolver.sourceExts.push('cjs');
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(defaultConfig, {
+  transformer: {
+    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+  },
+});
